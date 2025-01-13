19c10e97b1308b717b04f472206f62d90958ccbf
Vrajshah0306 <Vrajshah0306@yahoo.com>
2025-01-13 01:07:12 -0500

    app.js commit one

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
+const a = "X";
+const b = "O";
+var last =b;
+var count= 0;
+var X_points = 0;
+var O_points =0;
+function test(id)
+{
+    const Current = document.getElementById(id);
+    if (Current.textContent !== "") //Checking if a mark has been placed there or not.
+    {
+        return;
+    }
+    
+    if(this.last == a)
+    {
+        Current.style.fontFamily = "'Pacifico', cursive"; //font 
+        Current.style.color = "blue";  //color
+        Current.style.size = "1000px";
+        Current.innerHTML = b;
+        this.last=b;
+        document.getElementById("nextturn").textContent = "Current turn: Player " + a;
+        return;
+    }
+    else{
+       Current.style.color = "red";
+        Current.innerHTML = a;
+        this.last=a;
+        document.getElementById("nextturn").textContent = "Current turn: Player " + b;
+        return;
 
+    }
+}
+
+
+function resetboard()
+{
+    const squares = document.querySelectorAll(".square");
+    let i = 0;
+    while(i<squares.length)
+    {
+        squares[i].textContent = "";
+        i++; 
+    }
+    document.getElementById("nextturn").textContent = "Current turn: Player X";
+}
+function resetpoints()
+{
+    const PlayerX = document.getElementById("PlayerX");
+    const PlayerO = document.getElementById("PlayerO");
+    const draw =document.getElementById("Draw");
+    draw.textContent = "Draw: ";
+    PlayerX.textContent ="Player X: ";
+    PlayerO.textContent ="Player O: ";
+}
